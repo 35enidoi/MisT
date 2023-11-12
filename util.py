7bc8437d4f6180d2be51d4d4d678d189e2d232a0
いおぢね <143810964+35enidoi@users.noreply.github.com>
2023-11-12 09:55:06 +0900

    utilに情報追加
    get_instance_iconも移したかったけどrequestsのexceptiuon使ってるとこ多すぎてやめた

--- util.py
+++ util.py
@@ -1,4 +1,10 @@
 def makeqr(url,width,height):
+    """
+    QRコードを作る関数
+
+    optionalのqrcodeがあればQRコードを作る
+
+    ただしスクリーンのサイズが小さいとそのまま"""
     try:
         import qrcode
         qr = qrcode.QRCode(
@@ -30,5 +36,9 @@ def makeqr(url,width,height):
         return space.split("\n")[0]+space.join([url[i*lens:(i+1)*lens] for i in range(lines)])
 
 def webshow(url):
+    """
+    WEBを開く関数
+    
+    デスクトップ環境がない場合動作しないことを確認済み(Raspberry Pi OS Lite(Bullseye) : Raspberrypi Zero 2 W)(SSH接続)"""
     import webbrowser
     webbrowser.open(url)
