7c725d3757605f971e4bca7a3f882dbdae0c907a
いおぢね <143810964+35enidoi@users.noreply.github.com>
2023-12-04 19:59:48 +0900

    bootstrap.pyの中身を移動
    misskey_tui.pyで起動するようになる

--- misskey_tui.py
+++ misskey_tui.py
@@ -1299,5 +1299,27 @@ def main():
         except ResizeScreenError as e:
             last_scene = e.scene
 
+def init_translation():
+    import gettext
+    # 翻訳ファイルを配置するディレクトリ
+    path_to_locale_dir = os.path.abspath(
+        os.path.join(
+            os.path.dirname(__file__),
+            './locale'
+        )
+    )
+
+    # 翻訳用クラスの設定
+    translater = gettext.translation(
+        'messages',                   # domain: 辞書ファイルの名前
+        localedir=path_to_locale_dir, # 辞書ファイル配置ディレクトリ
+        languages=['ja_JP'],          # 翻訳に使用する言語
+        fallback=True                 # .moファイルが見つからなかった時は未翻訳の文字列を出力
+    )
+
+    # Pythonの組み込みグローバル領域に_という関数を束縛する
+    translater.install()
+
 if __name__ == "__main__":
+    init_translation()
     main()
