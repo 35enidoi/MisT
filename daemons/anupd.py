fb647e329cb04bad40c6553f0f9bab022518065f
いおぢね <143810964+35enidoi@users.noreply.github.com>
2024-01-23 23:06:22 +0900

    #35 ノートが100個たまってる時に先頭に行くと切るように変更
    つまり...自動更新！

--- daemons/anupd.py
+++ daemons/anupd.py
@@ -20,11 +20,23 @@ class Anupd:
         @Daemon.glb_getter
         def __anupd_ope(msk, _):
             if len(msk.notes) != 0:
-                btmnoteid = msk.notes[len(msk.notes)-1]["id"]
+                CUTNOTELEN = 20
+                CUTPOINT = 10
+                ntlen = len(msk.notes)
+                flag = False
+                if ntlen == 100 and msk.nowpoint <= CUTPOINT:
+                    ntlen = 100-CUTNOTELEN
+                    flag = True
+                btmnoteid = msk.notes[ntlen-1]["id"]
                 notes = msk.get_note(sinceid=btmnoteid[:-2]+"aa")
                 if notes is not None:
-                    ints = {i["id"] for i in msk.notes}&{i["id"] for i in notes}
-                    if len(ints) >= len(msk.notes)-2 and len(ints) != 0:
+                    beforenotes = msk.notes
+                    if flag:
+                        beforenotes = beforenotes[:100-CUTNOTELEN]
+                    ints = {i["id"] for i in beforenotes}&{i["id"] for i in notes}
+                    if len(ints) >= len(beforenotes)-2 and len(ints) != 0:
+                        if flag:
+                            msk.nowpoint += CUTNOTELEN
                         msk.nowpoint += len(notes)-len(msk.notes)
                         msk.notes = notes
 
