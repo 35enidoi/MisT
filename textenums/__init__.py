b46dd087da7993a9d6845c7cc462ad06afef32e4
いおぢね <143810964+35enidoi@users.noreply.github.com>
2024-03-14 17:56:02 +0900

    ファイルの名前間違えてた #44

--- textenums/__init__.py
+++ textenums/__init__.py
@@ -1,9 +1,9 @@
 from .noteview_txts import NV_T
 from .cfgmenu_txts import CM_T
 from .createnote_txts import CN_T
-from .createnotecfg_txt import CNC_T
+from .createnotecfg_txts import CNC_T
 
 __all__ = (noteview_txts.__all__+
            cfgmenu_txts.__all__ +
            createnote_txts.__all__+
-           createnotecfg_txt.__all__)
+           createnotecfg_txts.__all__)
